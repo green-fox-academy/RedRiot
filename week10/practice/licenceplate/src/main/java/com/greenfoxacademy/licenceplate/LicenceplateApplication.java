5022dc9805d76c3be69d3db9a4a9439fb98ea3e6
RedRiot <quoc.tung@outlook.com>
2018-01-29 13:26:27 +0100

    init project

---
+++ week10/practice/licenceplate/src/main/java/com/greenfoxacademy/licenceplate/LicenceplateApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.licenceplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenceplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenceplateApplication.class, args);
+	}
+}
