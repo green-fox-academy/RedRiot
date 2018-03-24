dd129f342e9498f81424c89a944b263db0bb7191
RedRiot <quoc.tung@outlook.com>
2018-03-24 13:00:44 +0100

    refactor method

--- PracticeExam/Pallida-Trial/Odd_filter/src/OddFilter.java
+++ PracticeExam/Pallida-Trial/Odd_filter/src/OddFilter.java
@@ -3,31 +3,19 @@ import java.util.Arrays;
 import java.util.List;
 
   public class OddFilter {
-
     public static void main(String[] args) {
-      // Create a function that takes a list as a parameter,
-      // and returns a new list with every odd element from the orignal list
+
       System.out.println(oddFilter(Arrays.asList(1, 2, 3, 4, 5)));
-      // should print [1, 3, 5]
     }
 
     private static List oddFilter(List<Integer> integers) {
       List<Integer> content = new ArrayList<>();
-      //paratlan szamok
-     /* for (int i = 0; i <integers.size() ; i++) {
+
+      for (int i = 0; i < integers.size(); i++) {
         if (integers.get(i) % 2 != 0) {
           content.add(integers.get(i));
         }
-        }
-      return content;*/
-
-
-     //paratlan elemek
-      for (int i = 0; i <integers.size() ; i+=2) {
-
-          content.add(integers.get(i));
-        }
-
+      }
       return content;
     }
   }
